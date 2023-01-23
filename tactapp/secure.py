a92bbadc1cc31478ad8e3076eacee08c3df6e9c7
Ashdof <mzigah4@gmail.com>
2023-01-23 20:04:26 +0000

    Created a file for user authentication

---
+++ tactapp/secure.py
@@ -0,0 +1,16 @@
+"""
+    =======================     TACT APPLICATION     ===================================
+    FILE:                   USER AUTHENTICATION CLASS
+    DATE:                   23-JAN-2023
+    LAST UPDATED:           23-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    DESCRIPTION:            THIS IS THE IMPORVED VERSION OF THE MYPRONETWORK APPLICATION
+
+    CLASS DESCRIPTION:      THIS CLASS PROVIDES USER AUTHENTICATION
+"""
+
+#!/usr/bin/python3
+
+class UserSecure:
+    """User Authentication CLass"""
