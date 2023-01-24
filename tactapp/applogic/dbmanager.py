34f6b5118ccf53584e87884ba16df80047856a5c
Ashdof <mzigah4@gmail.com>
2023-01-24 21:05:30 +0000

    Created a new file for dbmanager class and methods

---
+++ tactapp/applogic/dbmanager.py
@@ -0,0 +1,13 @@
+"""
+    =======================     TACT DATABASE MANAGER     ===================================
+    FILE:                   DATABASE ACCESS CLASS
+    DATE:                   24-JAN-2023
+    LAST UPDATED:           24-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+    DESCRIPTION:            THIS CLASS FILE HAS METHODS TO CONNECT TO THE DATABASE AND PERFORM CRUD ACTIONS ON IT. IT ACTS
+                            AS THE GLUE BETWEEN THE DATABASE FILE AND FRONTEND CLASS THAT INTERACTS WITH THE USER.
+"""
+
+#!/usr/bin/python3
+
+import sqlite3
