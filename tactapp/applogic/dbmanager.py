5db2d4904b0956c29343da62849e500019a8417a
Ashdof <mzigah4@gmail.com>
2023-01-24 23:30:46 +0000

    Reorganised files

--- tactapp/applogic/dbmanager.py
+++ tactapp/applogic/dbmanager.py
@@ -11,7 +11,7 @@
 #!/usr/bin/python3
 
 import sqlite3
-from base.tactbase import TactRoot
+from applogic.tactroot import TactRoot
 
 class TactdbManager(TactRoot):
     """Database management module"""
