a2074fa922d59a08e0e782b58a67aeea87b1a5a5
Ashdof <mzigah4@gmail.com>
2023-01-24 22:25:38 +0000

    New package
    Created a new package inside the applogic package to serve as the root
    package of the application. In this package is the following file:
    
    root.py module
    This module is the root of application. It hosts methods that initialise
    th application and manage database

---
+++ tactapp/applogic/base/root.py
@@ -0,0 +1,14 @@
+"""
+    =======================     TACT DATABASE MANAGER     ===================================
+    FILE:                   ROOT MODULE
+    DATE:                   24-JAN-2023
+    LAST UPDATED:           24-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+    DESCRIPTION:            THIS MODULE IS THE ROOT OF THE APPLICATION. IT IS RESPONSIBLE FOR:
+                            1. DATABASE CREATION AND MANAGEMENT
+                            2. DATABASE TABLE CREATION AND MANAGEMENT
+                            3. DATABASE ID MANAGEMENT
+                            4. EXPORT AND IMPORT OF DATABASE
+"""
+
+#!/usr/bin/python3
