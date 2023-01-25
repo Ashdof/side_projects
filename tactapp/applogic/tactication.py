3018b2433a9df4c3afc8526bd68228bc93140537
Ashdof <mzigah4@gmail.com>
2023-01-25 16:06:18 +0000

    Error Checking Integrity
    Minor fixes as explained below:
    
    Added another block of else if statement to the username setter
    method. This will check for the length of the new username to be
    assigned; the method raises a value error if the length of the
    new username is less than 3 characters.
    
    Also, spaced out lines of codes to enhance reading

--- tactapp/applogic/tactication.py
+++ tactapp/applogic/tactication.py
@@ -17,11 +17,11 @@ class Tactication(TactRoot):
 
     def __init__(self, username="", password="", id=None):
         """Initialise module
-        
+
         Description:
             This method initialises the module with the assigned values. The
             parameters can be omitted upon invocation
-        
+
         Args:
             username (str): username of this user and the first parameter
             password (str): password of this user and the second parameter
@@ -30,28 +30,28 @@ class Tactication(TactRoot):
         self.__username = username
         self.__password = password
         super().__init__(id)
-    
+
     @property
     def username(self):
         """Get username
-        
+
         Description:
             This method obtains the username of the user. It is executed by
             invoking the function through a dot-notation scheme without the
             parenthesis. Example:
 
             variable = authentication_variable.username
-        
+
         Returns:
             The username of this user
         """
 
         return self.__username
-    
+
     @username.setter
     def username(self, newname):
         """Update username
-        
+
         Description:
             This method sets a new username for this user. It can be assigned
             a value via the dot-notation. Example
@@ -60,34 +60,38 @@ class Tactication(TactRoot):
 
             The new_username value should rather be in quotes (single or double)
             and not the < and >
-        
+
         Raises:
             ValueError if a blank value is assigned to the method
 
         """
 
         if newname == " ":
-            raise TypeError(" '{}' cannot be used as a username".format(newname))
+            raise ValueError("'{}' cannot be used as a username".format(newname))
+
+        elif len(newname) < 3:
+            raise ValueError("Username should be at least three charaters long.")
+
         else:
             self.__username = newname
-    
+
     @property
     def password(self):
         """Get password
-        
+
         Description:
             This method obtains the password of this user. It is executed by
             invoking the function through a dot-notation scheme without the
             parenthesis. Example:
 
             variable = authentication_variable.password
-        
+
         Returns:
             Password of this user
         """
 
         return self.__password
-    
+
     @password.setter
     def password(self, newpass):
         """Update password
@@ -101,14 +105,16 @@ class Tactication(TactRoot):
 
             The new_password value should rather be in quotes (single or double)
             and not the < and >
-        
+
         Args:
             newpass (str): the new value to set the password
-        
+
         Raises:
             ValueError if the length of the new password is less than 6 characters
         """
+
         if len(newpass) < 6:
             raise ValueError("Password must be at least 6 characters long")
+
         else:
             self.__password = newpass
