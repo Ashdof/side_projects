4f903395b012874a62c03eb94c1f3d7ba928d3fc
Ashdof <mzigah4@gmail.com>
2023-01-30 15:36:40 +0000

    Changed date of last update to current

--- tactapp/applogic/tactication.py
+++ tactapp/applogic/tactication.py
@@ -2,7 +2,7 @@
     =======================     TACT APPLICATION     ===================================
     FILE:                   USER AUTHENTICATION MODULE
     DATE CREATED:           25-JAN-2023
-    LAST UPDATED:           28-JAN-2023
+    LAST UPDATED:           30-JAN-2023
     DEVELOPER:              EMMANUEL ENCHILL
 
     MODULE DESCRIPTION:     THIS MODULE PROVIDES USER AUTHENTICATION METHODS
