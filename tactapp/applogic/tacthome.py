105f53418b23e6a5a6a2b1dfa842fd90d9f7df67
Ashdof <mzigah4@gmail.com>
2023-01-30 16:47:36 +0000

    Created a new file to be the application's main home page

---
+++ tactapp/applogic/tacthome.py
@@ -0,0 +1,12 @@
+"""
+    =======================     TACT APPLICATION     ===================================
+    FILE:                   MAIN APPLICATION MODULE
+    DATE CREATED:           25-JAN-2023
+    LAST UPDATED:           30-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    MODULE DESCRIPTION:     THIS MODULE ENABLES THE USER TO INTERACT WITH ALL THE OTHER PARTS
+                            OF THE APPLICATION
+"""
+
+#!/usr/bin/env python3
