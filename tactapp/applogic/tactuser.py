5f6642521de028dbd1395867f3babed64769a8b2
Ashdof <mzigah4@gmail.com>
2023-01-31 22:15:32 +0000

    Created a new file. This module will have methods for managing the information of the user's contacts

---
+++ tactapp/applogic/tactuser.py
@@ -0,0 +1,12 @@
+"""
+    =======================     TACT APPLICATION     ===================================
+    FILE:                   USER DIRECT MODULE
+    DATE CREATED:           31-JAN-2023
+    LAST UPDATED:           31-JAN-2023
+    DEVELOPER:              EMMANUEL ENCHILL
+
+    MODULE DESCRIPTION:     THIS MODULE PROVIDES METHODS FOR HANDLING THE INFORMATION OF 
+                            THE USER'S CONTACTS
+"""
+
+#!/usr/bin/env python3
