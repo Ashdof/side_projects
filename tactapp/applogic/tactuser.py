96fc729c17b81cefd915f0868b033f345e002aed
Ashdof <mzigah4@gmail.com>
2023-01-31 22:53:55 +0000

    Duplicated and edited the methods for managing lastname for the first name of this contact information

--- tactapp/applogic/tactuser.py
+++ tactapp/applogic/tactuser.py
@@ -127,3 +127,44 @@ class TactUserDirect:
             raise ValueError("Can't assign last name to empty value")
         else:
             self.__lastname = new_name
+    
+    @property
+    def firstname(self):
+        """Last name
+
+        Description:
+            This method obtains the first name of this contact information. It can be
+            accessed by using the dot notation. Example:
+
+            variable = tactuser_variable.firstname
+        
+        Returns:
+            The first name of this contact
+        """
+
+        return self.__firstname
+    
+    @lastname.setter
+    def firstname(self, new_name):
+        """Update last name
+        
+        Description:
+            This method updates the first name of this contact information. It can be
+            assigned by a dot notation. Example:
+        
+            variable.firstname = <new_name>
+
+            The new_code value should rather be in quotes (single or double)
+            and not the < and >
+        
+        Raises:
+            TypeError if type of first_name is not a string
+            ValueError if the length of new_name variable is zero
+        """
+
+        if not isinstance(new_name, str):
+            raise TypeError("'{}' must a string of characters")
+        elif len(new_name == 0):
+            raise ValueError("Can't assign first name to empty value")
+        else:
+            self.__firstname = new_name
