05d8a8a6b5c80aebe6c6cd212c7a3764153c89d5
Ashdof <mzigah4@gmail.com>
2023-02-05 21:17:14 +0000

    Changed date of last update to current. Also, executed programme to test which updated the database

--- tactapp/applogic/tacthome.py
+++ tactapp/applogic/tacthome.py
@@ -2,7 +2,7 @@
     =======================     TACT APPLICATION     ===================================
     FILE:                   MAIN APPLICATION MODULE
     DATE CREATED:           25-JAN-2023
-    LAST UPDATED:           30-JAN-2023
+    LAST UPDATED:           05-FEB-2023
     DEVELOPER:              EMMANUEL ENCHILL
 
     MODULE DESCRIPTION:     THIS MODULE ENABLES THE USER TO INTERACT WITH ALL THE OTHER PARTS
