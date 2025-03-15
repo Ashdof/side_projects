212d8933581faac917c4139b9e8dfdf9a3ebfd1e
Emmanuel Enchill <mzigah4@gmail.com>
2025-03-15 17:27:56 +0000

    Delete ashgallery directory

--- ashgallery/src/main/resources/db/sql/create-table.sql
+++
@@ -1,9 +0,0 @@
--- Script to manage database table
-
-CREATE TABLE IF NOT EXISTS ashgallery (
-    id VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(50),
-    description VARCHAR(250),
-    image_path VARCHAR(255),
-    image_status SMALLINT DEFAULT 1
-);
