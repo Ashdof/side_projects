f3e8f94f004a49df757f4df848b74bacda9e38b1
Emmanuel Enchill <mzigah4@gmail.com>
2025-04-28 12:59:13 +0000

    Created an empty class for root application context configuration.

---
+++ ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/config/CatalogueRootApplicationContextConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Configuration for database connectivity
+ *
+ * @author Emmanuel Enchill
+ */
+
+package com.enchill_projects.ashnovel_catalogue.config;
+
+public class CatalogueRootApplicationContextConfig {
+
+    // DataSource Configurations
+    // Hibernate Configurations
+    // PasswordEncoder Configurations etc.
+}
