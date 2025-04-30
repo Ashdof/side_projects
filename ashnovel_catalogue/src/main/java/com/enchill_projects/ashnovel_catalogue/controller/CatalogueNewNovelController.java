2ee57816f09c35f6ef0ab8dece452e90c3828e51
Emmanuel Enchill <mzigah4@gmail.com>
2025-04-30 11:20:05 +0000

    Wired the Novel Serice and created a method to process form data

--- ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/controller/CatalogueNewNovelController.java
+++ ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/controller/CatalogueNewNovelController.java
@@ -6,15 +6,27 @@
 package com.enchill_projects.ashnovel_catalogue.controller;
 
 import com.enchill_projects.ashnovel_catalogue.domain.Novel;
+import com.enchill_projects.ashnovel_catalogue.service.CatalogueNovelService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("catalogue")
 public class CatalogueNewNovelController {
+
+    private final CatalogueNovelService catalogueNovelService;
+
+    @Autowired
+    public CatalogueNewNovelController(CatalogueNovelService catalogueNovelService) {
+        this.catalogueNovelService = catalogueNovelService;
+    }
 
     /**
      * New Novel:
@@ -28,5 +40,26 @@ public class CatalogueNewNovelController {
         model.addAttribute("newNovel", new Novel());
 
         return "new_novel";
+    }
+
+    /**
+     * Process Form:
+     * process the form values and commit to the database
+     * @param novel the new Novel object
+     * @param bindingResult the binding object
+     * @return to the home page if successful, otherwise remain on
+     * the form if there are errors
+     */
+    @PostMapping("/new_novel")
+    public String processNewNovelForm(
+            @ModelAttribute("novel") Novel novel,
+            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors())
+            return "new_novel";
+
+        catalogueNovelService.saveNovelRecord(novel);
+
+        return "redirect:/catalogue/home";
     }
 }
