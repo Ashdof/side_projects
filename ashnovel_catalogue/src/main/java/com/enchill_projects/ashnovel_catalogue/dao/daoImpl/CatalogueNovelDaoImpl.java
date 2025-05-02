edd5b755f0089ca4d99976e862b4765fc4a2f8b8
Emmanuel Enchill <mzigah4@gmail.com>
2025-05-02 14:50:46 +0000

    Implemented the method to fetch all novel objects

--- ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/dao/daoImpl/CatalogueNovelDaoImpl.java
+++ ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/dao/daoImpl/CatalogueNovelDaoImpl.java
@@ -12,6 +12,8 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class CatalogueNovelDaoImpl implements CatalogueNovelDao {
 
@@ -30,5 +32,23 @@ public class CatalogueNovelDaoImpl implements CatalogueNovelDao {
     public void save(CatalogueNovel catalogueNovel) {
         Session session = sessionFactory.getCurrentSession();
         session.persist(catalogueNovel);
+    }
+
+    /**
+     * All Novels:
+     * get all Novel objects from the database
+     * @return a list of all Novel objects
+     */
+    public List<CatalogueNovel> fetchAllNovels() {
+        Session session = sessionFactory.openSession();
+        List<CatalogueNovel> catalogueNovels;
+
+        try {
+            catalogueNovels = session.createQuery("FROM CatalogueNovel", CatalogueNovel.class).list();
+        } finally {
+            session.clear();
+        }
+
+        return catalogueNovels;
     }
 }
