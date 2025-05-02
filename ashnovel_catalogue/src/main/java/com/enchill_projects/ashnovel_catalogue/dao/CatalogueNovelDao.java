bab58806efb0cb6680b579a3f2b70dc49d6bf578
Emmanuel Enchill <mzigah4@gmail.com>
2025-05-02 14:31:54 +0000

    Added a method to fetch all novels

--- ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/dao/CatalogueNovelDao.java
+++ ashnovel_catalogue/src/main/java/com/enchill_projects/ashnovel_catalogue/dao/CatalogueNovelDao.java
@@ -8,6 +8,9 @@ package com.enchill_projects.ashnovel_catalogue.dao;
 
 import com.enchill_projects.ashnovel_catalogue.domain.CatalogueNovel;
 
+import java.util.List;
+
 public interface CatalogueNovelDao {
     void save(CatalogueNovel catalogueNovel);
+    List<CatalogueNovel> fetchAllNovels();
 }
